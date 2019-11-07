8fe55c436909c5948ae1726e25043b1afd25ec41
Alex Trottier <postables@rtradetechnologies.com>
2019-11-06 20:46:24 -0800

    add some more stuff

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+// delcare a boolean
+var isDone = false;
+// declare a number (floating point value)
+var decimal = 1.1;
+// delcare a string
+var color = "red";
+color = 'purple';
+// template strings can span multiple lines and have embedded expressions
+// sorrounded by backticks and embedded expressisons are in the form of
+// ${ expr }
+var phrase = "This is a color " + color;
+console.log(phrase);
+// function declaration
 function greeter(person) {
     return "Hello " + person + "!";
 }
--- main.ts
+++ main.ts
@@ -1,7 +1,22 @@
+// delcare a boolean
+let isDone: boolean = false;
+// declare a number (floating point value)
+let decimal: number = 1.1;
+// delcare a string
+let color: string = "red";
+color = 'purple';
+// template strings can span multiple lines and have embedded expressions
+// sorrounded by backticks and embedded expressisons are in the form of
+// ${ expr }
+let phrase: string = `This is a color ${color}`
+console.log(phrase);
+
+
+// function declaration
 function greeter(person: string) {
 	return `Hello ${person}!`
 }
 
-let p = greeter("alex");
 
+let p = greeter("alex");
 console.log(p);
